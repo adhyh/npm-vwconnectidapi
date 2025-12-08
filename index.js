8e6f6e3f049008dd14a1404ed73f7ee6a6cad59e
Adhy <20784602+adhyh@users.noreply.github.com>
2025-12-08 20:39:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,6 @@ class VwWeConnect {
         if (this.vinArray.includes(pVin)) {
             this.currSession.vin = pVin;
             this.log.info("Active VIN successfully set to <" + this.currSession.vin + ">.");
-            this.setDatabase("10.55.0.1");
         } else {
             this.log.error(
                 "VIN <" + pVin + "> is unknown. Active VIN is still <" + this.currSession.vin + ">."
